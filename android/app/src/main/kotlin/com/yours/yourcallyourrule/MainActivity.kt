ec746aa286db61a5cf2a81f332f7bfae84a2e458
oakgeol <oakgeol@gmail.com>
2024-11-13 07:57:49 +0800

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/yours/yourcallyourrule/MainActivity.kt
+++ android/app/src/main/kotlin/com/yours/yourcallyourrule/MainActivity.kt
@@ -15,7 +15,6 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 class MainActivity : FlutterActivity() {
 
