b673976b070bf0fac465aa61327da263e2a53515
haygcao <ngokelcho@outlook.com>
2025-06-02 20:15:36 +0800

    clean

--- wuyong/lib clean architecture 架构/domain/entities/rule/phone_number_rule.dart
+++
@@ -1,9 +0,0 @@
-import '../../value_objects/phone_number.dart';
-
-/// 电话号码规则接口
-/// 用于标识包含电话号码的规则类型
-abstract class PhoneNumberRule {
-  PhoneNumber get phoneNumber;
-  String? get label;
-  String? get avatar;
-}
