9fd2b7832834f9f571f8c71d10769bd39d392673
haygcao <ngokelcho@outlook.com>
2025-12-13 23:39:31 +0800

    chore: remove unused dependencies and plugins
    Clean up project by removing unused packages and plugins to reduce bundle size and improve maintainability. This includes removing connectivity_plus, geolocator, share_plus, and other unused dependencies.

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,15 +4,12 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
-  connectivity_plus
   file_selector_windows
   firebase_auth
   firebase_core
   flutter_inappwebview_windows
   flutter_secure_storage_windows
-  geolocator_windows
   permission_handler_windows
-  share_plus
   sqlite3_flutter_libs
   url_launcher_windows
 )
