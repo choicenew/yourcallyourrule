b2a3c8d67d895eddbd2dcfb2fa7804b78a0510ee
haygcao <ngokelcho@outlook.com>
2026-01-22 23:48:24 +0800

    test 新 flutterjs 模式

---
+++ ztest/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_inappwebview_macos
+import flutter_js
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
+}
